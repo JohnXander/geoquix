d789690e6704e1322fb9cc66430e000d16224aa6
John Xander <johnblox5@gmail.com>
2023-01-27 15:10:26 +0000

    add km2 unicode

--- src/pages/capitals.tsx
+++ src/pages/capitals.tsx
@@ -34,9 +34,11 @@ const Capitals = () => {
             <Header />
             
             <main className="flex flex-col items-center py-4 gap-4">
-                <h2>Capital city?</h2>
-                <p>{score} / 10</p>
-                <p>{quizLen} questions left</p>
+                <div className="flex flex-col gap-4 md:flex-row items-center">
+                    <h2 className="text-2xl">What is the capital city?</h2>
+                    <p className="border-2 py-4 px-8">{score} / 10</p>
+                    <p>{quizLen} questions left</p>
+                </div>
 
                 {quizLen > 0 &&
                     <div className="flex flex-col items-center justify-center border-y-2 w-full py-6 px-8">
