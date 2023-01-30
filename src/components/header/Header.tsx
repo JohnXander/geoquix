3f4ce263f5847134308fa3c626383d2177057295
John Xander <johnblox5@gmail.com>
2023-01-30 16:16:01 +0000

    change theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "../../../assets/logo.png"
 
 export const Header = () => {
     return (
-        <div className="p-2 md:px-12 lg:px-32 border-b-2 border-grey-500">
+        <div className="p-4 md:px-12 lg:px-32 border-b-2 border-grey-500">
             <div className="flex items-center gap-2 w-fit">
                 <Image
                     src={Logo}
@@ -11,7 +11,9 @@ export const Header = () => {
                     width={40}
                     height={40}
                 />
-                <h1 className="text-3xl font-bold">GeoQuix</h1>
+                <h1 className="text-3xl font-bold">
+                    Geo<b className="text-cyan-500">Quix</b>
+                </h1>
             </div>
         </div>
     )
