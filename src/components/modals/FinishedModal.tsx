d2a1c13a6ad66c521d78852abad8b9bae5ecb83c
John Xander <johnblox5@gmail.com>
2023-01-31 12:05:35 +0000

    add accuracy to finishedModal

--- src/components/modals/FinishedModal.tsx
+++ src/components/modals/FinishedModal.tsx
@@ -5,11 +5,14 @@ interface Props {
 
 const FinishedModal: React.FC<Props> = ({ score, answered }) => {
 
+    const percentage = Math.floor((100 * score) / answered)
+
     return (
         <div className="bg-gray-800 flex flex-col items-center border-2 border-gray-700 rounded py-32 px-8 absolute">
             <h1 className="text-3xl">Finished Quiz!</h1>
             <p className="text-2xl">Questions: {answered}</p>
             <p className="text-2xl">Score: {score}</p>
+            <p className="text-2xl">Accuracy: {percentage}%</p>
         </div>
     )
 }
