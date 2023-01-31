6bdec29ee7b7de2293b6844317f31cc0ace4bfc6
John Xander <johnblox5@gmail.com>
2023-01-31 20:46:15 +0000

    display database data in leaderboard

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -3,32 +3,20 @@ import { procedure, router } from '../trpc';
 import { prisma } from '../utils/prisma';
 
 export const appRouter = router({
-  // createUser: procedure
-  // .input(z.object({ username: z.string(), email: z.string(), password: z.string() }),)
-  // .mutation(async ({ input }) => {
-  //   const createdUser = await prisma.user.create({ data: { ...input } });
-  //   return {success: true, newUser: createdUser};
-  // }),
+  
+  getAllScores: procedure
+    .query(async () => {
+      const foundScores = await prisma.score.findMany();
+      return foundScores
+    }),
 
   createScore: procedure
     .input(z.object({ name: z.string(), score: z.number(), accuracy: z.number(), quiz: z.string() }),)
     .mutation(async ({ input }) => {
       const createdScore = await prisma.score.create({ data: { ...input } })
-      return {newScore: createdScore}
+      return { createdScore }
     }),
-  
 
-  // hello: procedure
-  //   .input(
-  //     z.object({
-  //       text: z.string(),
-  //     }),
-  //   )
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `hello ${input.text}`,
-  //     };
-  //   }),
 });
 
 export type AppRouter = typeof appRouter;
